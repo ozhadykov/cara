ab5ad7df24a414617c3407c3a27feaa8ac93167e
Omar Zhadykov <omar3282088@gmail.com>
2025-05-03 15:10:11 +0200

    slightly changed sidebar

--- frontend/app/src/components/layout/Sidebar.tsx
+++ frontend/app/src/components/layout/Sidebar.tsx
@@ -18,10 +18,11 @@ const Sidebar = () => {
       <div className="drawer-side">
         <label htmlFor="my-drawer" aria-label="close sidebar" className="drawer-overlay"></label>
         <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
-          <div className="sidebar-header border-b-2 border-zinc-400 py-2">
+          <div className="sidebar-header border-b-2 border-zinc-400 pb-2.5 flex flex-col">
             <span className="text-xl font-bold">CARA</span>
+            <span className="pl-0.5 text-xs font-light text-zinc-400">your smart child care assistant</span>
           </div>
-          <li className="sidebar-body py-3">
+          <li className="sidebar-body py-3 text-base">
             {links}
           </li>
         </ul>
