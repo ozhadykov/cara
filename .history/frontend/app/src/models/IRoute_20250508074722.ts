f483530b27a2e24ba742c3636fa331de6fa73d38
Omar Zhadykov <omar3282088@gmail.com>
2025-05-08 13:25:30 +0200

    deleted .history

--- .history/frontend/app/src/models/IRoute_20250508074722.ts
+++
@@ -1,8 +0,0 @@
-import { ReactNode } from "react";
-
-export interface IRoute {
-  path: string;
-  label: string;
-  component: ReactNode; // Verwende ReactNode
-  icon?: ReactNode; // Verwende ReactNode
-}
